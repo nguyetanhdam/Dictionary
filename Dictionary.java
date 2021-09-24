eeebe0eb426d7d95d2f1e49bf761fe00bb6f6f79
DNAnh <20020129@vnu.edu.vn>
2021-09-24 22:13:36 +0700

    command line sơ khai

---
+++ Dictionary.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Dictionary {
+    public ArrayList<Word> dict = new ArrayList<>();
+
+    public ArrayList<Word> getDict() {
+        return dict;
+    }
+
+    public void setDict(ArrayList<Word> dict) {
+        this.dict = dict;
+    }
+}
