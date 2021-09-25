1ac157add760f203e266bb962ec73626aa118de9
pgk15 <phamkhiem1502@gmail.com>
2021-09-25 10:19:00 +0700

    CommandLine1 hoàn thiện

--- DictionaryCommandline.java
+++ DictionaryCommandline.java
@@ -1,11 +1,10 @@
 import java.util.ArrayList;
 
 public class DictionaryCommandline {
-    public void showAllWords() {
+    public static void showAllWords() {
         int i = 1;
         DictionaryManagement dim = new DictionaryManagement();
         ArrayList<Word> WordList = dim.getWordList();
-        dim.insertFromCommandline();
 
         System.out.println("No  | English            | Vietnamese" + '\n');
         for (Word word : WordList) {
@@ -15,12 +14,8 @@ public class DictionaryCommandline {
         }
     }
 
-    public void dictionaryBasic() {
-
-    }
-
-    public static void main(String[] args) {
-        DictionaryCommandline dcl = new DictionaryCommandline();
-        dcl.showAllWords();
+    public static void dictionaryBasic() {
+        DictionaryManagement.insertFromCommandline();
+        showAllWords();
     }
 }
