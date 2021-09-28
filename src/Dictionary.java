2eab7b3b4211e079fca0bb51919b05e8b6707c02
pgk15 <phamkhiem1502@gmail.com>
2021-09-28 23:16:12 +0700

    Binary Search

--- src/Dictionary.java
+++ src/Dictionary.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 
 public class Dictionary {
-    public ArrayList<Word> dict = new ArrayList<>();
+    public static ArrayList<Word> dict = new ArrayList<>();
 
     public ArrayList<Word> getDict() {
         return dict;
@@ -9,5 +9,18 @@ public class Dictionary {
 
     public void setDict(ArrayList<Word> dict) {
         this.dict = dict;
+    }
+
+    public static Word binaryLookup(int start, int end, String eng) {
+        if(end<start) {
+            return null;
+        }
+        int mid= start + (end-start)/2;
+        Word word= dict.get(mid);
+        String s= word.getWord_target();
+        int compare = s.compareTo(eng);
+        if(compare ==0) return word;
+        if(compare > 0) return binaryLookup(start, mid-1, eng);
+        return binaryLookup(mid+1,end, eng);
     }
 }
