f02c7c25db2bde2a36000ca2663fdb96f9f05ead
DNAnh <20020129@vnu.edu.vn>
2021-10-27 20:36:07 +0700

    final commit

--- src/main/java/com/example/test/Main.java
+++ src/main/java/com/example/test/Main.java
@@ -15,6 +15,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
         try {
+            primaryStage.setTitle("Dictionary");
             Parent root= FXMLLoader.load(getClass().getResource("sample.fxml"));
             Scene scene = new Scene(root);
             primaryStage.setScene(scene);
